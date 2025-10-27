d3d50a432a785bbddb2919356a839d45ef48da81
yusuf601 <yusufmuhyusuh@gmail.com>
2025-10-27 21:37:40 +0700

    chore: update shell script and implementation

--- build_testing.sh
+++ build_testing.sh
@@ -1,6 +1,13 @@
 build(){
+        # clean build directory
+    rm -rf build
+
+    # reconfigure and build
     cmake -S . -B build
     cmake --build build -j
+
+    # run tests
     ctest --test-dir build --timeout 5 -V
+
 }
 build
