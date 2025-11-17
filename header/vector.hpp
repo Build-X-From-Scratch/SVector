799b29c673dd14f1aa77ed0ff6eb149f9b5b50f7
yusuf601 <yusufmuhyusuh@gmail.com>
2025-11-17 09:38:15 +0700

    feat(header): optimize grow

--- header/vector.hpp
+++ header/vector.hpp
@@ -409,7 +409,7 @@ class Vector{
         Iterator insert(const_iterator pos,std::size_t n,const T&& val){
             ssize_t offset = pos - begin();
             if(size + n >= capacity){
-                grow(n * 2);
+                grow(size + n);
             }
             ssize_t end = offset;
             if(is_empty()){
